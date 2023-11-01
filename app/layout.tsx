015fb8cd27d8bb2f9476b8359551e653803ca60c
ankoi0310 <huynhvahuuan3620@gmail.com>
2023-11-01 13:57:47 +0700

    add ads.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
         <Script
           async
           src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4452476152277124'
+          strategy='lazyOnload'
           crossOrigin='anonymous'
         />
       </head>

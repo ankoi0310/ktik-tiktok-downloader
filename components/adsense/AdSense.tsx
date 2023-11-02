a1d9adf6b0ab2e6f5a97dc7707556434cede90b5
ankoi0310 <huynhvahuuan3620@gmail.com>
2023-11-03 00:04:15 +0700

    fix ads component

--- components/adsense/AdSense.tsx
+++ components/adsense/AdSense.tsx
@@ -1,32 +1,24 @@
-import React, { useEffect } from 'react'
+import Script from 'next/script'
+import React from 'react'
 
 type AdSenseProps = {
   adSlot: string
 }
 
 const AdSense = ({ adSlot }: AdSenseProps) => {
-  useEffect(() => {
-    try {
-      // @ts-ignore
-      (window.adsbygoogle = window.adsbygoogle || []).push({})
-    } catch (err) {
-      console.log(err)
-    }
-  }, [])
-  
   return (
     <>
       <ins
-        className='adsbygoogle adbanner-customize'
+        className='adsbygoogle'
         style={{ display: 'block' }}
         data-ad-client='ca-pub-4452476152277124'
         data-ad-slot={adSlot}
         data-ad-format='auto'
         data-full-width-responsive='true'
       ></ins>
-      {/*<script>*/}
-      {/*  (adsbygoogle = window.adsbygoogle || []).push({});*/}
-      {/*</script>*/}
+      <Script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      </Script>
     </>
   )
 }
